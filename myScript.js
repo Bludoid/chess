4c4ae9126c9b8310c7509de2115a8b7eb85382fd
Bludoid <79199839+Bludoid@users.noreply.github.com>
2024-09-30 19:29:12 +0200

    cleaned the code, added comments

--- myScript.js
+++ myScript.js
@@ -1,13 +1,23 @@
-// todo:  switch between black and white player
+///////////////////////////////////////////
+//   chess board in HTML/CSS/JavaScript  //
+///////////////////////////////////////////
+
+
+//        to do:  
 //        be able to take any opponent's piece (update board and array)
-//  move a pawn properly
+//        move a pawn properly
+//        move counter or even notation (also show next to the board)
+//        visual representation of who's move it is (next to the board as a square or strip in the right colour)
 
-
+// for creating the board (and its white/black pieces)
 let colorCounter = 1;
 let board = document.getElementById("board");
 let whiteMove = true;         // true => white's move, false => black's move
-let whiteInCheck = false;    // if true => check or checkmate happening
-let blackInCheck = false;
+
+// not used for yet:
+// let whiteInCheck = false;    // if true => check or checkmate happening
+// let blackInCheck = false;
+
 let moveInProgress = false;
 let chosenSquare = null;
 let boardSituation = [
@@ -21,6 +31,9 @@ let boardSituation = [
                       ["p", "w"], ["p", "w"], ["p", "w"], ["p", "w"], ["p", "w"], ["p", "w"], ["p", "w"], ["p", "w"], 
                       ["r", "w"], ["n", "w"], ["b", "w"], ["q", "w"], ["k", "w"], ["b", "w"], ["n", "w"], ["r", "w"]
                     ]
+
+
+
 
 // create the board
 for(let i = 0; i<64; i++) {
@@ -62,29 +75,33 @@ for(let i = 0; i<64; i++) {
   
 }
 
-// place a piece on the board given square ID (1 to 64) and shortcut of the piece ("bp" - black pawn)
+// place a piece on the board given the square and shortcut of the piece ("bp" - black pawn)
 function placePiece(square, pieceName) {
-  //let desiredSquare = document.getElementById(squareID);
   let newPiece = document.createElement("img");
-  let pathString = "img/" + pieceName + ".png";
+  let pathString = "img/" + pieceName + ".png";   // puts together the name of the image
   newPiece.src = pathString;
   newPiece.className = pieceName;
   square.appendChild(newPiece);
 }
 
+// removes any piece from the given square by clearing its inner HTML
 function removePiece(square) {
   square.innerHTML = "";
 
 }
 
+// highlights the given square by adding a class, highlighting happens in CSS
 function highlightSquare(square) {
   square.classList.add("highlighted");
 }
 
+// removes the class/highlight from the given square
 function unhighlightSquare(square) {
   square.classList.remove("highlighted");
 }
 
+// checks if the square contains a piece and if it belongs to the player who's turn it is - if yes, returns true
+// if it's not the right player's turn or the square is empty, returns false
 function isPlayersTurn(square) {
   if (boardSituation[square.id][1] == "w" && whiteMove) {
     return true;
@@ -93,13 +110,10 @@ function isPlayersTurn(square) {
     return true;
   }
   else {
+    // returns false if it's not player's move or if the clicked square is empty
     return false;
   }
 }
-
-
-
-
 
 // return a string with colour and piece ("white rook")
 function recognizePiece(pieceShortcut) {
@@ -142,6 +156,7 @@ function recognizePiece(pieceShortcut) {
   return recognizedPiece;
 }
 
+// sets up the chessboard by placing the pieces in their correct starting positions
 function setUpBoard() {
   for (let i = 1; i<17; i++) {
     let pieceName = boardSituation[i][0] + boardSituation[i][1];
@@ -153,36 +168,36 @@ function setUpBoard() {
   }
 }
 
-// movePossible() if color fits procede to clicking another piece if valid, call executeMove()
-
+// starts the move by setting moveInProgress to true, highlights the given square and saves the square as chosenSquare 
 function possibleMove(square) {
-  // if not check, check for validity of the desired move, if cool, call executeMove()
   console.log("There is this piece on the square: " + square.firstChild.className);
   console.log(recognizePiece(square.firstChild.className));
   moveInProgress = true;
-  chosenSquare = square;
   highlightSquare(square);
+  chosenSquare = square;
 }
 
+// executes the move of a piece from given oldSquare to newSquare
+// updates the board state in boardSituation
+// switches the turn to the other player (in whiteMove)
 function executeMove(oldSquare, newSquare) {
-  //let pickedSquare = document.getElementById(squareID);
+  // updates boardSituation array
   console.log("executing move");
-  //console.log("There is this piece on the square: " + square.firstChild.className);
-  //console.log(recognizePiece(square.firstChild.className));
   boardSituation[newSquare.id][0] = boardSituation[oldSquare.id][0]; 
   boardSituation[oldSquare.id][0] = "e";
   boardSituation[newSquare.id][1] = boardSituation[oldSquare.id][1]; 
   boardSituation[oldSquare.id][1] = "e";
+  
+  // moves the square
   placePiece(newSquare, oldSquare.firstChild.className);
+  
+  // cleans up after the move
   removePiece(oldSquare);
   moveInProgress = false;
   chosenSquare = null;
   unhighlightSquare(oldSquare);
   whiteMove = !whiteMove;  // toggling white/black player move
-  console.log(boardSituation);
-  // calls updateBoard()
 }
 
-
-
+// call the function to initially set up the board
 setUpBoard();
