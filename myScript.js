e42b3133853e32cde9be82cf777f15ed4232675c
Bludoid <79199839+Bludoid@users.noreply.github.com>
2024-10-09 13:28:14 +0200

    combined boardSituation and boardCoordinates arrays into boardRepresentation
    the boardRepresentation array has 4 members for each of squares ['r', 'b', 'a', 8] for piece, color, row, column (rook black a8)... or ['e', 'e', 'c', 6] is empty ("e") square c6

--- myScript.js
+++ myScript.js
@@ -20,30 +20,47 @@ let infobox = document.getElementById("infobox");
 
 let moveInProgress = false;
 let chosenSquare = null;
-let boardSituation = [
-                      [],     // index 0 is empty to have same numbers as ID's and in this array
-                      ["r", "b"], ["n", "b"], ["b", "b"], ["q", "b"], ["k", "b"], ["b", "b"], ["n", "b"], ["r", "b"], 
-                      ["p", "b"], ["p", "b"], ["p", "b"], ["p", "b"], ["p", "b"], ["p", "b"], ["p", "b"], ["p", "b"],
-                      ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], 
-                      ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"],
-                      ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"],
-                      ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"],
-                      ["p", "w"], ["p", "w"], ["p", "w"], ["p", "w"], ["p", "w"], ["p", "w"], ["p", "w"], ["p", "w"], 
-                      ["r", "w"], ["n", "w"], ["b", "w"], ["q", "w"], ["k", "w"], ["b", "w"], ["n", "w"], ["r", "w"]
-                    ];
 
 
-const boardCoordinates = [
-  [],   // zero member is not used
-  ["a", 8], ["b", 8], ["c", 8], ["d", 8], ["e", 8], ["f", 8], ["g", 8], ["h", 8],
-  ["a", 7], ["b", 7], ["c", 7], ["d", 7], ["e", 7], ["f", 7], ["g", 7], ["h", 7],
-  ["a", 6], ["b", 6], ["c", 6], ["d", 6], ["e", 6], ["f", 6], ["g", 6], ["h", 6],
-  ["a", 5], ["b", 5], ["c", 5], ["d", 5], ["e", 5], ["f", 5], ["g", 5], ["h", 5],
-  ["a", 4], ["b", 4], ["c", 4], ["d", 4], ["e", 4], ["f", 4], ["g", 4], ["h", 4],
-  ["a", 3], ["b", 3], ["c", 3], ["d", 3], ["e", 3], ["f", 3], ["g", 3], ["h", 3],
-  ["a", 2], ["b", 2], ["c", 2], ["d", 2], ["e", 2], ["f", 2], ["g", 2], ["h", 2],
-  ["a", 1], ["b", 1], ["c", 1], ["d", 1], ["e", 1], ["f", 1], ["g", 1], ["h", 1]
-  ];
+// let boardSituation = [
+//                       [],     // index 0 is empty to have same numbers as ID's and in this array
+//                       ["r", "b"], ["n", "b"], ["b", "b"], ["q", "b"], ["k", "b"], ["b", "b"], ["n", "b"], ["r", "b"], 
+//                       ["p", "b"], ["p", "b"], ["p", "b"], ["p", "b"], ["p", "b"], ["p", "b"], ["p", "b"], ["p", "b"],
+//                       ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], 
+//                       ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"],
+//                       ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"],
+//                       ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"], ["e", "e"],
+//                       ["p", "w"], ["p", "w"], ["p", "w"], ["p", "w"], ["p", "w"], ["p", "w"], ["p", "w"], ["p", "w"], 
+//                       ["r", "w"], ["n", "w"], ["b", "w"], ["q", "w"], ["k", "w"], ["b", "w"], ["n", "w"], ["r", "w"]
+//                     ];
+
+
+// const boardCoordinates = [
+//                           [],   // zero member is not used
+//                           ["a", 8], ["b", 8], ["c", 8], ["d", 8], ["e", 8], ["f", 8], ["g", 8], ["h", 8],
+//                           ["a", 7], ["b", 7], ["c", 7], ["d", 7], ["e", 7], ["f", 7], ["g", 7], ["h", 7],
+//                           ["a", 6], ["b", 6], ["c", 6], ["d", 6], ["e", 6], ["f", 6], ["g", 6], ["h", 6],
+//                           ["a", 5], ["b", 5], ["c", 5], ["d", 5], ["e", 5], ["f", 5], ["g", 5], ["h", 5],
+//                           ["a", 4], ["b", 4], ["c", 4], ["d", 4], ["e", 4], ["f", 4], ["g", 4], ["h", 4],
+//                           ["a", 3], ["b", 3], ["c", 3], ["d", 3], ["e", 3], ["f", 3], ["g", 3], ["h", 3],
+//                           ["a", 2], ["b", 2], ["c", 2], ["d", 2], ["e", 2], ["f", 2], ["g", 2], ["h", 2],
+//                           ["a", 1], ["b", 1], ["c", 1], ["d", 1], ["e", 1], ["f", 1], ["g", 1], ["h", 1]
+//                           ];
+
+
+// array or arrays with empty member on index 0
+// 64 squares represented as ['r', 'b', 'a', 8] (piece, color, row, column), indexes 0 and 1 possibly "e" for empty square
+let boardRepresentation = [
+  [],
+  ['r', 'b', 'a', 8], ['n', 'b', 'b', 8], ['b', 'b', 'c', 8], ['q', 'b', 'd', 8], ['k', 'b', 'e', 8], ['b', 'b', 'f', 8], ['n', 'b', 'g', 8], ['r', 'b', 'h', 8],
+  ['p', 'b', 'a', 7], ['p', 'b', 'b', 7], ['p', 'b', 'c', 7], ['p', 'b', 'd', 7], ['p', 'b', 'e', 7], ['p', 'b', 'f', 7], ['p', 'b', 'g', 7], ['p', 'b', 'h', 7], 
+  ['e', 'e', 'a', 6], ['e', 'e', 'b', 6], ['e', 'e', 'c', 6], ['e', 'e', 'd', 6], ['e', 'e', 'e', 6], ['e', 'e', 'f', 6], ['e', 'e', 'g', 6], ['e', 'e', 'h', 6],
+  ['e', 'e', 'a', 5], ['e', 'e', 'b', 5], ['e', 'e', 'c', 5], ['e', 'e', 'd', 5], ['e', 'e', 'e', 5], ['e', 'e', 'f', 5], ['e', 'e', 'g', 5], ['e', 'e', 'h', 5],
+  ['e', 'e', 'a', 4], ['e', 'e', 'b', 4], ['e', 'e', 'c', 4], ['e', 'e', 'd', 4], ['e', 'e', 'e', 4], ['e', 'e', 'f', 4], ['e', 'e', 'g', 4], ['e', 'e', 'h', 4],
+  ['e', 'e', 'a', 3], ['e', 'e', 'b', 3], ['e', 'e', 'c', 3], ['e', 'e', 'd', 3], ['e', 'e', 'e', 3], ['e', 'e', 'f', 3], ['e', 'e', 'g', 3], ['e', 'e', 'h', 3],
+  ['p', 'w', 'a', 2], ['p', 'w', 'b', 2], ['p', 'w', 'c', 2], ['p', 'w', 'd', 2], ['p', 'w', 'e', 2], ['p', 'w', 'f', 2], ['p', 'w', 'g', 2], ['p', 'w', 'h', 2],
+  ['r', 'w', 'a', 1], ['n', 'w', 'b', 1], ['b', 'w', 'c', 1], ['q', 'w', 'd', 1], ['k', 'w', 'e', 1], ['b', 'w', 'f', 1], ['n', 'w', 'g', 1], ['r', 'w', 'h', 1]
+ ]
 
 // create the board and add click ability to the squares
 for(let i = 0; i<64; i++) {
@@ -82,9 +99,9 @@ for(let i = 0; i<64; i++) {
     }
     else if (isEmptySquare(thisSquare)) {
       // a square is hightlighted and player clicked empty square ==> if valid, a move happening
-        if (isValidMove(thisSquare))  {
-      executeMove(chosenSquare, thisSquare);
-      console.log("moving to an empty square");
+      if (isValidMove(thisSquare))  {
+        executeMove(chosenSquare, thisSquare);
+        console.log("moving to an empty square");
       }
     }
   })
@@ -132,10 +149,10 @@ function unhighlightSquare(square) {
 // checks if the square contains a piece and if it belongs to the player who's turn it is - if yes, returns true
 // if it's not the right player's turn or the square is empty, returns false
 function isPlayersTurnAndPiece(square) {
-  if (boardSituation[square.id][1] == "w" && whiteMove) {
+  if (boardRepresentation[square.id][1] == "w" && whiteMove) {
     return true;
   }
-  else if (boardSituation[square.id][1] == "b" && !whiteMove) {
+  else if (boardRepresentation[square.id][1] == "b" && !whiteMove) {
     return true;
   }
   else {
@@ -145,13 +162,13 @@ function isPlayersTurnAndPiece(square) {
 }
 
 function isEmptySquare(square) {
-  if (boardSituation[square.id][0] == "e") {
+  if (boardRepresentation[square.id][0] == "e") {
     return true;
   }
 }
 
 function isOpponentsPiece(square) {
-  if ((!whiteMove && boardSituation[square.id][1] == "w") || (whiteMove && boardSituation[square.id][1] == "b")) {
+  if ((!whiteMove && boardRepresentation[square.id][1] == "w") || (whiteMove && boardRepresentation[square.id][1] == "b")) {
     return true;  
   }
 }
@@ -174,7 +191,7 @@ function takePiece(takingPieceSquare, takenPieceSquare) {
 
 // converts square ID to actual chess board coordinates (1 => a8, 64 => h1, ...)
 function coordinatesOfSquare(squareID) {
-  return (boardCoordinates[squareID][0] + boardCoordinates[squareID][1]);
+  return (boardRepresentation[squareID][2] + boardRepresentation[squareID][3]);
 }
 
 // outputs message for players to the infobox
@@ -183,11 +200,11 @@ function outputToInfobox(infoMessage) {
 }
 
 function isValidMove(square) {
-  let pieceSchortcut = boardSituation[chosenSquare.id][0];
+  let pieceSchortcut = boardRepresentation[chosenSquare.id][0];
   console.log(pieceSchortcut);
   switch (pieceSchortcut) {
     case "p": 
-        if (boardSituation[chosenSquare.id][1] == "w") {return isWhitePawnMove(square)}
+        if (boardRepresentation[chosenSquare.id][1] == "w") {return isWhitePawnMove(square)}
         else {return isBlackPawnMove(square)}
     case "r":
       return isRookMove(square); 
@@ -246,8 +263,8 @@ function isKingMove(square) {
 // return a string with colour and piece ("white rook")
 function pieceToString(square) {
   let recognizedPiece = "";
-  let pieceColor = boardSituation[square.id][1];
-  let pieceSchortcut = boardSituation[square.id][0];
+  let pieceColor = boardRepresentation[square.id][1];
+  let pieceSchortcut = boardRepresentation[square.id][0];
   
   
   // is piece black or white?
@@ -290,11 +307,11 @@ function pieceToString(square) {
 // sets up the chessboard by placing the pieces in their correct starting positions
 function setUpBoard() {
   for (let i = 1; i<17; i++) {
-    let pieceName = boardSituation[i][0] + boardSituation[i][1];
+    let pieceName = boardRepresentation[i][0] + boardRepresentation[i][1];
     placePiece(document.getElementById(i), pieceName);
   }
   for (let i = 49; i<65; i++) {
-    let pieceName = boardSituation[i][0] + boardSituation[i][1];
+    let pieceName = boardRepresentation[i][0] + boardRepresentation[i][1];
     placePiece(document.getElementById(i), pieceName);
   }
 }
@@ -309,21 +326,21 @@ function markPossibleMove(square) {
 }
 
 // executes the move of a piece from given oldSquare to newSquare
-// updates the board state in boardSituation
+// updates the board state in boardRepresentation
 // switches the turn to the other player (in whiteMove)
 function executeMove(oldSquare, newSquare) {
-  // updates boardSituation array
+  // updates boardRepresentation array
   console.log("executing move");
   
   // moves the piece to the new square
-  placePiece(newSquare, (boardSituation[oldSquare.id][0] +  boardSituation[oldSquare.id][1]));
+  placePiece(newSquare, (boardRepresentation[oldSquare.id][0] +  boardRepresentation[oldSquare.id][1]));
 
-  // make an updateBoardSituation function?????? : 
-  // updates the boardSituation array
-  boardSituation[newSquare.id][0] = boardSituation[oldSquare.id][0]; 
-  boardSituation[oldSquare.id][0] = "e";
-  boardSituation[newSquare.id][1] = boardSituation[oldSquare.id][1]; 
-  boardSituation[oldSquare.id][1] = "e";
+  // make an updateboardRepresentation function?????? : 
+  // updates the boardRepresentation array
+  boardRepresentation[newSquare.id][0] = boardRepresentation[oldSquare.id][0]; 
+  boardRepresentation[oldSquare.id][0] = "e";
+  boardRepresentation[newSquare.id][1] = boardRepresentation[oldSquare.id][1]; 
+  boardRepresentation[oldSquare.id][1] = "e";
   
   
   
